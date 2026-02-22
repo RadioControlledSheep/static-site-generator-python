369160f2edec208b753a39f37a1da637d6b7a586
RadioControlledSheep <joeydorz@gmail.com>
2026-02-22 12:26:21 +0000

    Added unit tests

---
+++ src/test_textnode.py
@@ -0,0 +1,62 @@
+import unittest
+
+from textnode import TextNode, TextType
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.TEXT_BOLD)
+        node2 = TextNode("This is a text node", TextType.TEXT_BOLD)
+        self.assertEqual(node, node2)
+
+    def test_not_eq(self):
+        node = TextNode("This is a text node", TextType.TEXT_BOLD)
+        node2 = TextNode("This is a text node", TextType.TEXT_ITALIC)
+        self.assertNotEqual(node, node2)
+
+    def test_default_url(self):
+        node = TextNode("This is a text node", TextType.TEXT_BOLD)
+        self.assertIs(node.url, None)
+
+    def test_text_not_eq(self):
+        node = TextNode("This is a text nod", TextType.TEXT_BOLD)
+        node2 = TextNode("This is a text node", TextType.TEXT_BOLD)
+        self.assertNotEqual(node, node2)
+
+    def test_no_text_eq(self):
+        node = TextNode("", TextType.TEXT)
+        node2 = TextNode("", TextType.TEXT)
+        self.assertEqual(node, node2)
+
+    def test_no_text_not_eq(self):
+        node = TextNode("", TextType.TEXT_BOLD)
+        node2 = TextNode("", TextType.TEXT_ITALIC)
+        self.assertNotEqual(node, node2)
+
+    def test_url_eq(self):
+        node = TextNode(
+            "This is a text node", TextType.TEXT_BOLD, "http://www.boot.dev"
+        )
+        node2 = TextNode(
+            "This is a text node", TextType.TEXT_BOLD, "http://www.boot.dev"
+        )
+        self.assertEqual(node, node2)
+
+    def test_url_not_eq(self):
+        node = TextNode(
+            "This is a text node", TextType.TEXT_BOLD, "http://www.boot.dev/"
+        )
+        node2 = TextNode(
+            "This is a text node", TextType.TEXT_BOLD, "http://www.boot.dev"
+        )
+        self.assertNotEqual(node, node2)
+
+    def test_repr(self):
+        node = TextNode("This is a text node", TextType.TEXT, "https://www.boot.dev")
+        self.assertEqual(
+            "TextNode(This is a text node, text, https://www.boot.dev)", repr(node)
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
