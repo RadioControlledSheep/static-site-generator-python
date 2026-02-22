57c7ddbb9d37a44aea9a03c283bb80757d1747d9
RadioControlledSheep <joeydorz@gmail.com>
2026-02-22 12:25:47 +0000

    Added unit tests

---
+++ src/textnode.py
@@ -0,0 +1,28 @@
+from enum import Enum
+from tarfile import LENGTH_LINK
+
+
+class TextType(Enum):
+    TEXT = "text"
+    TEXT_BOLD = "bold"
+    TEXT_ITALIC = "italic"
+    TEXT_CODE = "code"
+    LINK = "link"
+    IMAGE = "image"
+
+
+class TextNode:
+    def __init__(self, text, text_type, url=None):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __eq__(self, other):
+        return (
+            self.text == other.text
+            and self.text_type == other.text_type
+            and self.url == other.url
+        )
+
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
