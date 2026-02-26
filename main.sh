5afc09dad621cd99a828f0de6432092b8f12fd2b
RadioControlledSheep <joeydorz@gmail.com>
2026-02-26 17:44:13 +0000

    hopefully working now

--- main.sh
+++ main.sh
@@ -1,2 +1,2 @@
-python3 src/main.py ./docs
+python3 src/main.py
 cd docs && python3 -m http.server 8888
--- src/main.py
+++ src/main.py
@@ -6,10 +6,9 @@ from generate_html import generate_pages_recursive
 
 def main():
     if len(sys.argv) != 2:
-        exit(1)
-        # copy_files_recursive("./static", "./docs")
-        # generate_pages_recursive("./content", "template.html", "./docs")
-        # sys.exit(0)
+        copy_files_recursive("./static", "./docs")
+        generate_pages_recursive("./content", "template.html", "./docs", "/")
+        sys.exit(0)
     # book_filepath = "books/frankenstein.txt"
     basepath = sys.argv[1]
     copy_files_recursive("./static", "./docs")
